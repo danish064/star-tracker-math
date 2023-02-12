b08f83a3dfbd0a3771831f3218a198b4b0961ce5
danish2016 <danish.danish.danish2016@gmail.com>
2023-02-12 23:24:53 +0500

    Updated arduino code

--- accel_stepper.ino
+++ accel_stepper.ino
@@ -1,26 +1,25 @@
 #include <AccelStepper.h>
-#define FULLSTEP 4
-#define HALFSTEP 8
-
-const int operationMode = FULLSTEP;
-
-long stepsPerRev = 2038;  //steps per stepper motor revolution
-const float rpm = 10;
-const float rps = (stepsPerRev * rpm) / 60;
 
 #define motorPin1 8   // IN1 on the ULN2003 driver 1
 #define motorPin2 9   // IN2 on the ULN2003 driver 1
 #define motorPin3 10  // IN3 on the ULN2003 driver 1
 #define motorPin4 11  // IN4 on the ULN2003 driver 1
 
+#define FULLSTEP 4
+#define HALFSTEP 8
+
+const int operationMode = FULLSTEP;
+long stepsPerRev = 2038;  //steps per stepper motor revolution
+const float rpm = 10;
+float rps = (stepsPerRev * rpm) / 60;
+
 AccelStepper myStepper(operationMode, motorPin1, motorPin3, motorPin2, motorPin4);  // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
 long totalSteps = 0;
 
 void runMotor() {
-  if (myStepper.runSpeed() && totalSteps <= stepsPerRev) {  // This will run the motor forever.
-    totalSteps++;
-    Serial.println(totalSteps);
-    // Serial.println("Stepping");
+  // Serial.println(myStepper.currentPosition());
+  if (myStepper.currentPosition() % stepsPerRev == 0) {
+    Serial.println("Completed " + String(myStepper.currentPosition() / stepsPerRev) + " revolutions");
   }
 }
 void checkSerial() {
@@ -28,24 +27,33 @@ void checkSerial() {
     // read the incoming byte:
     char incomingByte = Serial.read();
 
-    // say what you got:
     Serial.print("I received: ");
     Serial.println(incomingByte);
   }
 }
 void setup() {
   Serial.begin(9600);
-  if (operationMode == HALFSTEP)
+  if (operationMode == HALFSTEP) {
     stepsPerRev = stepsPerRev + stepsPerRev;
+    rps = rps + rps;
+  }
   Serial.print("Total Steps: ");
   Serial.println(stepsPerRev);
-
+  Serial.println(rps);
   myStepper.setMaxSpeed(stepsPerRev);  // this limits the value of setSpeed(). Raise it if you like.
   myStepper.setSpeed(rps);             // runSpeed() will run the motor at this speed - steps per second
+  // myStepper.setAcceleration(100);
 }
 
 
 void loop() {
-
+  myStepper.runSpeed();
   runMotor();
+  // myStepper.runSpeed();
+  // Serial.println(myStepper.currentPosition());
+  // if (myStepper.run()) {
+  //   Serial.println("Still moving");
+  // } else {
+  //   Serial.println("1 rev complete");
+  // }
 }
